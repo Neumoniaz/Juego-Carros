22f5003eda1eaa93987f2fe9c46cabf4c17d8a28
Neumoniaz <niariver18000@gmail.com>
2026-09-07 12:47:08 -0500

    Menu principal funcional y menu pausa
    Menu principal con sus botones y pausa con el esc con reanudar, reiniciar y main menu :D.

---
+++ Pista de carros/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("UI Panel")]
+    public GameObject pauseMenuPanel; 
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f; 
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f; 
+        isPaused = false;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(0); 
+    }
+}
